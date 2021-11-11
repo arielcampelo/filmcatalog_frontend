04a8f4679a9f7565dd975a701d5bfec9081aba1e
José Lopes Moreira Filho <jlopes@alumni.usp.br>
2021-11-11 11:55:38 -0300

    Before material design

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
-    <Navbar/>
     <router-view/>
-    <Footer/>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
 
 export default {
-  components: {
-    Navbar,
-    Footer
-  }
 }
 </script>
 <style>
